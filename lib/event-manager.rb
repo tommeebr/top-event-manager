85810469649bd99ad238f076be4b96bb0505f906
tommeebr <tommyb1651@gmail.com>
2023-10-10 19:02:42 +0100

    Cleaning the zipcodes

--- lib/event-manager.rb
+++ lib/event-manager.rb
@@ -8,6 +8,12 @@ contents = CSV.open('event_attendees.csv', headers: true, header_converters: :sy
 
 contents.each do |row|
     name = row[:first_name]
-    zipcode = row[:zipcode]
-    puts "#{name} #{zipcode}"
+    zipcode = row[:zipcode].to_s.dup
+    if zipcode.length < 5
+        len = 5 - zipcode.length
+        zipcode = zipcode.prepend("0" * len)
+    elsif zipcode.length > 5
+        zipcode = zipcode.slice(0,5)
+    end
+    puts ("#{name} #{zipcode}")
 end
