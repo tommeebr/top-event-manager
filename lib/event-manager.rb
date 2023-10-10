b7cffbb9ffba034f95878b90118e53033dbb614f
tommeebr <tommyb1651@gmail.com>
2023-10-10 19:20:37 +0100

    Corrected Zip Code clear-up

--- lib/event-manager.rb
+++ lib/event-manager.rb
@@ -1,4 +1,18 @@
 require 'csv'
+
+def clean_zipcode(zipcode)
+    if zipcode.nil?
+        zipcode = '00000'
+    elsif zipcode.length < 5
+        len = 5 - zipcode.length
+        zipcode = zipcode.prepend("0" * len)
+    elsif zipcode.length > 5
+        zipcode = zipcode.slice(0..4)
+    else
+        zipcode
+    end
+end
+
 puts 'Event Manager Initialised!'
 
 
@@ -6,14 +20,11 @@ lines = File.readlines("event_attendees.csv")
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
+
+
 contents.each do |row|
     name = row[:first_name]
-    zipcode = row[:zipcode].to_s.dup
-    if zipcode.length < 5
-        len = 5 - zipcode.length
-        zipcode = zipcode.prepend("0" * len)
-    elsif zipcode.length > 5
-        zipcode = zipcode.slice(0,5)
-    end
+    zipcode = row[:zipcode]
+    zipcode = clean_zipcode(zipcode)
     puts ("#{name} #{zipcode}")
 end
